72eed3ee35f9813c67c4d86882ad0b3355af4434
Lukas Senger <lukas@fridolin.com>
2014-11-19 22:57:02 +0100

    Clean up debugging output

--- pi3g-netconf.go
+++ pi3g-netconf.go
@@ -30,49 +30,49 @@ const (
 // restart a service (e.g. hostapd, tor)
 func restartService(service string) error {
 	out, err := exec.Command(serviceBin, service, "restart").CombinedOutput()
-	debugf("restarting %s: %s \n", service, out)
+	debugf("restarting %s:\n%s", service, out)
 	return err
 }
 
 // stop a service (e.g. hostapd, tor)
 func stopService(service string) error {
 	out, err := exec.Command(serviceBin, service, "stop").CombinedOutput()
-	debugf("stop %s: %s \n", service, out)
+	debugf("stop %s:\n%s", service, out)
 	return err
 }
 
 // start a service (e.g. hostapd, tor)
 func startService(service string) error {
 	out, err := exec.Command(serviceBin, service, "start").CombinedOutput()
-	debugf("starting %s: %s \n", service, out)
+	debugf("starting %s:\n%s", service, out)
 	return err
 }
 
 // add an ip address to an interface
 func ipAddr(iface, subnet string) error {
 	out, err := exec.Command(ipBin, "addr", "add", "192.168."+subnet+".1/24", "broadcast", "192.168."+subnet+".255", "dev", iface).CombinedOutput()
-	debugf("setting ip on %s(%s): %s \n", iface, subnet, out)
+	debugf("setting ip on %s(%s):\n%s", iface, subnet, out)
 	return err
 }
 
 // remove all ip addresses from an interface
 func ipFlush(iface string) error {
 	out, err := exec.Command(ipBin, "addr", "flush", "dev", iface).CombinedOutput()
-	debugf("flushing addresses of %s: %s \n", iface, out)
+	debugf("flushing addresses of %s:\n%s", iface, out)
 	return err
 }
 
 // bring interface up
 func ipUp(iface string) error {
 	out, err := exec.Command(ipBin, "link", "set", "up", iface).CombinedOutput()
-	debugf("bringing %s up: %s \n", iface, out)
+	debugf("bringing %s up:\n%s", iface, out)
 	return err
 }
 
 // bring interface down
 func ipDown(iface string) error {
 	out, err := exec.Command(ipBin, "link", "set", "down", iface).CombinedOutput()
-	debugf("bringing %s down: %s \n", iface, out)
+	debugf("bringing %s down:\n%s", iface, out)
 	return err
 }
 
@@ -137,35 +137,34 @@ func main() {
 	if iface == "eth1" {
 		subnet = "43"
 	}
-	debugf("IFACE=%s\n", iface)
 	iface = os.Getenv("INTERFACE")
 	if iface == "wlan0" {
 		subnet = "42"
 	} else if iface == "eth1" {
 		subnet = "43"
 	}
-	debugf("INTERFACE=%s\n", iface)
 	if subnet == "" {
 		debug("Not our interface.")
 		os.Exit(0)
 	}
-	debugf("Subnet: %s", subnet)
 
 	// was it added or removed
 	action := NONE
 	mode := os.Getenv("MODE")
-	debugf("MODE=%s\n", mode)
 	if mode == "start" {
 		action = START
 	} else if mode == "stop" {
 		action = STOP
 	}
 	mode = os.Getenv("ACTION")
-	debugf("ACTION=%s\n", mode)
 	if mode == "add" {
 		action = START
 	} else if mode == "remove" {
 		action = STOP
+	}
+	if action == NONE {
+		debug("Unknown action.")
+		os.Exit(0)
 	}
 
 	// change tor config
@@ -174,15 +173,11 @@ func main() {
 		err = up(subnet)
 	} else if action == STOP {
 		err = down(subnet)
-	} else {
-		debug("Unknown action.")
-		os.Exit(0)
 	}
 	if err != nil {
 		debug(err)
 		os.Exit(1)
 	}
-	debug(os.Getenv("PHASE"))
 
 	// restart the appropriate services
 	// Hostapd is rather finicky, which is why the ip configuration is done
